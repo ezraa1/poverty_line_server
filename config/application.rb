d69a533c98e3194c160e3d49841ed108016318e0
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-10-19 13:34:58 +0300

    add gem dependencies

--- config/application.rb
+++ config/application.rb
@@ -23,17 +23,9 @@ module PovertyLineServer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.action_dispatch.cookies_same_site_protection = :strict
     config.api_only = true
   end
 end
