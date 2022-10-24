12f7c12c1d9a0eca7f4c2eb575f55ef448011c0f
bonveeee <bonochieng@gmail.com>
2022-10-24 20:36:52 +0300

    [updates] on region-user serializers

--- app/serializers/region_serializer.rb
+++ app/serializers/region_serializer.rb
@@ -1,3 +1,6 @@
 class RegionSerializer < ActiveModel::Serializer
   attributes :id, :sub_region, :sub_region_code, :country_name, :country_code, :city, :city_reporting_level
+
+  has_many :users
+  has_many :donations through: :users
 end
