2182a6ccdcf5751182482cd2bef392f9b257e002
bonveeee <bonochieng@gmail.com>
2022-10-26 10:54:13 +0300

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_20_062504) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "donations", force: :cascade do |t|
     t.integer "user_id"
     t.string "donation_name"
@@ -48,12 +51,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_062504) do
     t.string "access_to_electricity"
     t.string "medical_insurance"
     t.string "education_level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_type"
     t.string "religion"
     t.boolean "is_disabled"
     t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
