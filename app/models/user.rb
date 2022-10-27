3ed149c0f64c28007c345fd40b2cfe9dfbf1a44a
bonveeee <bonochieng@gmail.com>
2022-10-28 00:35:38 +0300

    corss

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :donations
-    belongs_to :region
+   # belongs_to :region
     
     enum user_type: {admin: 0, regular: 1}
 
@@ -13,6 +13,6 @@ class User < ApplicationRecord
      uniqueness: true
      
     validates :password, length: { in: 6..80 }
-    validates :age, numericality: { only_integer: true }
+   # validates :age, numericality: { only_integer: true }
     validates :full_name, presence: true,  length: { minimum: 2 }
 end
